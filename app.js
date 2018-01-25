e25d2705d37c7134b5bd81cd87830a02cc27c206
Quentin <qgerard.gerard@gmail.com>
2018-01-25 20:13:46 +0200

    mongoose added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var bodyParser = require('body-parser');
 
 var book = require('./routes/book');
 var app = express();
+var mongoose = require('mongoose');
+// mongoose.Promise = require('bluebird');
+mongoose.connect('mongodb://GalaxyDefender:Qu3ntin1988@ds115758.mlab.com:15758/mean-books');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
